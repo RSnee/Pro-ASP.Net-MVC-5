d6353bb441ce53b6c510896b22fc54a5d6bca497
RSnee <sneesoft@gmail.com>
2016-07-13 22:01:04 +0200

    First reading block finished

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersport",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(this.myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(this.myProduct);
+        }
+    }
+}
